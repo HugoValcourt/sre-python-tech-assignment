f79df3e1f755140bf814a7228016578883a77793
Hugo Valcourt <hugo_valcourt@hotmail.com>
2020-12-04 10:27:06 -0500

    Step one done

--- sre-python-tech-assignment.py
+++ sre-python-tech-assignment.py
@@ -3,40 +3,36 @@ import json
 import requests
 from urllib.request import urlretrieve
 
+## Define our target url
+neows_url = "https://api.nasa.gov/neo/rest/v1/feed"
 mykey = "57mwuDVeXDdR9UtDLG4W9Lb7jPN65bggqplSr2FI"
-start_date = "2019-10-31"
-end_date = "2019-11-02"
 
-def fetchAsteroidNeowsFeed():
-  neows_url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2019-10-31&end_date=2019-11-02&detailed=false"
+## Step 1:
+##
+## Get asteroids between the start and end dates
+## into a dictionary with urllib's request and JSON
+
+def step1_fetchNeowsAsteroidData():
+
+  ## Define our parameters for the url
   params = {
-      'api_key':mykey,
-      'start_date':'2020-01-22',
-      'end_date':'2020-01-23'
+      "api_key":mykey,
+      "start_date":"2019-10-31",
+      "end_date":"2019-11-02"
   }
+
+  ## Get the data from NASA's feed
   response = requests.get(neows_url,params=params).json()
-  print(response)
-fetchAsteroidNeowsFeed()
 
-"""## Variable definition. I got the url from NeoWS Api's website
-neows_url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=STARTING_DATE&end_date=ENDING_DATE&detailed=true&api_key="
-mykey = "57mwuDVeXDdR9UtDLG4W9Lb7jPN65bggqplSr2FI"
+  ## For every day in our dictionary,
+  ## get every asteroid and print the desired info
+  ## 'name', 'id' and 'close_approach_date_full'
+  for date in response["near_earth_objects"]:
+    print(date + ": " + str(len(response["near_earth_objects"][date])) + " asteroids was found on that day")
+    for asteroid in range(len(response["near_earth_objects"][date])):      
+      print(f"""Asteroid {asteroid}:
+      Name: {response["near_earth_objects"][date][asteroid]["name"]}
+      ID: {response["near_earth_objects"][date][asteroid]["id"]}
+      Close approach date full: {response["near_earth_objects"][date][asteroid]["close_approach_data"][0]["close_approach_date_full"]}\n""")
 
-## Start and End dates could also be user input
-start_date = "2019-10-31"
-end_date = "2019-11-02"
-
-## Set the proper search date
-neows_url = neows_url.replace("STARTING_DATE", start_date)
-neows_url = neows_url.replace("ENDING_DATE", end_date)
-
-## Request our object from NeoWs
-neows_obj = urllib.request.urlopen(neows_url + mykey)
-
-## Get info from file
-neows_read = neows_obj.read()
-
-## Decode JSON to Python data
-decodeneows = json.loads(neows_read.decode("utf-8"))
-
-print(decodeneows)"""
+step1_fetchNeowsAsteroidData()
